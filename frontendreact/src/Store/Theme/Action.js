b58e394e3a3e802ed27316a963bdc70f8d933e05
suhana kousar <163721735+suhanakousar@users.noreply.github.com>
2025-05-11 07:17:31 +0530

    twittt

--- frontendreact/src/Store/Theme/Action.js
+++
@@ -1,9 +0,0 @@
-import { CHANGE_THEME } from "./ActionType"
-
-export const changeTheme=(theme)=>(dispatch)=>{
-
-    localStorage.setItem("theme",theme)
-
-    dispatch({type:CHANGE_THEME,payload:theme})
-
-}
